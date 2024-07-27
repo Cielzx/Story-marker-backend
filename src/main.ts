48675103f3d550d7eecda4932febaa488aad4dbf
Cielzx <santosjaciel30@gmail.com>
2024-07-27 12:09:47 -0300

    Localhost allowed into CORS

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,7 @@ async function bootstrap() {
   );
   app.enableCors({
     origin: [
+      'http://localhost:3000',
       'https://story-marker-backend.onrender.com',
       'https://story-makers-beta.vercel.app',
     ],
