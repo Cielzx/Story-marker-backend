55dc8520e96c16bd4bb0189263ba7621e0ee1295
Cielzx <santosjaciel30@gmail.com>
2024-08-28 12:06:25 -0300

    Revert "Fix: Comment on main seed function"
    This reverts commit 13eb4da3d727fb1670854e63ac552854cf52428e.

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -23,11 +23,6 @@ import { FileFieldsInterceptor } from '@nestjs/platform-express';
 export class UsersController {
   constructor(private readonly userService: UserService) {}
 
-  @Post('')
-  create(@Body() data: CreateUserDto) {
-    return this.userService.create(data);
-  }
-
   @Get('')
   findAll() {
     return this.userService.findAll();
